829dab0bab30a46fc36f54eb832770aceadc6958
TomerHalfon <TomerHalfon773@gmail.com>
2021-03-07 17:42:30 +0200

    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GamesModule } from './games/games.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { GamesModule } from './games/games.module';
   imports: [
     BrowserModule,
     GamesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
