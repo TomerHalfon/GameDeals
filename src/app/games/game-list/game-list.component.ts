af1b24514921e1f54017d828f999fcbc7fa06fb3
AvivBenY <benyedidia123@gmail.com>
2021-03-07 17:45:52 +0200

    game list update

--- src/app/games/game-list/game-list.component.ts
+++ src/app/games/game-list/game-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-export class GameListComponent implements OnInit {
+export class GameListComponent implements OnInit {  
+  @Input()
+  games: any[]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
 }
--- src/app/games/game-search/game-search.component.ts
+++ src/app/games/game-search/game-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-search',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-search.component.css']
 })
 export class GameSearchComponent implements OnInit {
+  @Input()
+  game:any
 
   constructor() { }
 
   ngOnInit(): void {
     
   }
-
+  
 }
